1dba78ff089f2f3c8c3d2890a76f671e6f039431
hello-worldyj <sis00011086@gmail.com>
2025-10-20 22:32:56 +0700

    Update service-worker.js

--- .github/workflows/service-worker.js
+++ .github/workflows/service-worker.js
@@ -1,15 +1,15 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("love-cache").then((cache) => {
-      return cache.addAll(["./", "./index.html", "./heart.png"]);
+    caches.open("love-test-v1").then((cache) => {
+      return cache.addAll(["/", "/index.html", "/manifest.json"]);
     })
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((res) => {
+      return res || fetch(event.request);
     })
   );
 });
